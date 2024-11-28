14cd163016f42d3afcb4a927714212b57c1a559d
shreyash776 <shreyashchaurasia77@gmail.com>
2024-11-28 15:29:14 +0530

    refactor database connection to use a separate module for MongoDB connection

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const mongoose = require('./config/database');
+const connectDB = require('./config/database');
 const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
 const app = express();
 
+// Connect to MongoDB
+connectDB();
 
 app.use(express.json());
 
-
+// Routes
 app.use('/api/auth', authRoutes);
-
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.error(err));
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
